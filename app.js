85ccd6d7d0577672f2b74cc301e0edbf17c34402
simoniful <75239459+simoniful@users.noreply.github.com>
2021-04-14 12:22:04 +0900

    2주차 점검

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 WebFontConfig = {
+  google: {
+    families: ['Hind']
+  },
   custom: {
   families: ['Nanum Gothic'],
   urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
   }
 };
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})(); 
   
 // 야간모드
 
